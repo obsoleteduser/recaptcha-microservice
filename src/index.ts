a9b97f0c54490d66c060864b2e9c2e547463f37e
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-02-05 21:50:45 +0400

    Built info page

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, {Request} from 'express';
+import express, {Request, Response} from 'express';
 import dotenv from 'dotenv';
 import  RecaptchaRoute from './routes/recaptcha.route';
 import cors from 'cors'
@@ -13,6 +13,11 @@ app.use(cors<Request>())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use('/recaptcha', RecaptchaRoute);
+app.get('/', (req: Request, res: Response)=>{
+      res.send(`<h1>Welcome to reCAPTCHA verification API!</h1>
+                <p>Use this endpoint for verifing: https://validation-microservice.onrender.com/recaptcha/verify</p>
+                   `)
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
