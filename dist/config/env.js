2ff4d336281b9b7b17bdf75d00b77d4967068a22
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-02-06 00:41:33 +0400

    fixed

--- dist/config/env.js
+++ dist/config/env.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RECAPTCHA_SECRET_KEY = exports.PORT = void 0;
-exports.PORT = process.env.PORT;
+exports.PORT = +process.env.PORT || 5000;
 exports.RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET;
 //# sourceMappingURL=env.js.map
--- dist/index.js
+++ dist/index.js
@@ -7,8 +7,8 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const recaptcha_route_1 = __importDefault(require("./routes/recaptcha.route"));
 const cors_1 = __importDefault(require("cors"));
+const env_1 = require("./config/env");
 dotenv_1.default.config();
-const port = process.env.PORT || 5000;
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
@@ -16,10 +16,10 @@ app.use(express_1.default.urlencoded({ extended: false }));
 app.use('/recaptcha', recaptcha_route_1.default);
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to Tahir's reCAPTCHA verification API!</h1>
-                <p><b>Use this endpoint for verifing:</b> https://validation-microservice.onrender.com/recaptcha/verify</p>
+                <p><b>Use this endpoint for verifying:</b> https://validation-microservice.onrender.com/recaptcha/verify</p>
                    `);
 });
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(env_1.PORT, () => {
+    console.log(`Server is running on port ${env_1.PORT}`);
 });
 //# sourceMappingURL=index.js.map
