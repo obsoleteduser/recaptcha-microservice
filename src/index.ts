00d19e6be628fabb671cdd8ad0faffd8140798c9
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-02-05 05:59:58 +0400

    INIT

--- src/index.ts
+++ src/index.ts
@@ -16,26 +16,6 @@ app.use(express.urlencoded({extended: false}))
 
 app.use('/recaptcha', RecaptchaRoute);
 
-
-app.get('/', (req, res)=>{
-    res.send(`
-    
-    <form action="/recaptcha/verify" method="post">
-  <div class="form-group">
-    <!-- form inputs here -->
-  </div>
-  <div class="form-group">
-    <div class="g-recaptcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div>
-  </div>
-  <button type="submit">Submit</button>
-</form>
-
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-
-    
-    `)
-})
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
