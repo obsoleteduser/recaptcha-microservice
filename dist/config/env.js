8990ee4c3fe0cf90323b5bf03c1dcae6f682feb7
Tahir Dibirov II <95382261+tahirdibirovresearch@users.noreply.github.com>
2023-02-08 00:28:02 +0400

    removed

--- dist/config/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RECAPTCHA_VERIFICATION_URL = exports.RECAPTCHA_SECRET_KEY = exports.PORT = void 0;
-exports.PORT = +process.env.PORT || 5000;
-exports.RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET;
-exports.RECAPTCHA_VERIFICATION_URL = process.env.RECAPTCHA_VERIFICATION_URL;
-//# sourceMappingURL=env.js.map
