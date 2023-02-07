1ca2e8b90eea7acd9a3c38ca04ef9278bc4ab4a9
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-02-07 20:35:24 +0400

    Geo IP

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -8,7 +8,7 @@ import geoip from 'geoip-lite'
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
 
 const logger = (req: Request, res: Response, next: NextFunction)=>{
-    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    const clientIp = String(req.headers['x-forwarded-for'] || req.connection.remoteAddress).split(',')[0]
     const geop = geoip.lookup(String(clientIp))
     const log = `${new Date().toISOString()} ${req.method} ${req.url} GEO: ${geop} IP: ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}`
     console.log(log)
