abcbb2a1d776cbada1ab7f26b87d4d8ea364aa8f
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-02-07 22:24:55 +0400

    #

--- dist/middleware/logger.js
+++ dist/middleware/logger.js
@@ -8,8 +8,8 @@ const path_1 = __importDefault(require("path"));
 const geoip_lite_1 = __importDefault(require("geoip-lite"));
 const accessLogStream = fs_1.default.createWriteStream(path_1.default.join(__dirname, 'access.log'), { flags: 'a' });
 const logger = (req, res, next) => {
-    let clientIp = req.headers['x-real-ip'];
-    const geop = geoip_lite_1.default.lookup(`${clientIp}`);
+    let clientIp = req.headers['x-forwarded-for'];
+    const geop = geoip_lite_1.default.lookup(`${clientIp}`.split(',')[0]);
     console.log(geoip_lite_1.default.lookup('35.230.106.76'));
     const log = `${new Date().toISOString()} ${req.method} ${req.url} GEO: ${geop === null || geop === void 0 ? void 0 : geop.country} IP: ${clientIp}`;
     console.log(log);
