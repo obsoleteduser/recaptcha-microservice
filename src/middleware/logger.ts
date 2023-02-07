a8478c3e43059282266b94894abfcc9f7deed1c3
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-02-07 22:31:11 +0400

    almost done

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -10,8 +10,7 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'),
 const logger = (req: Request, res: Response, next: NextFunction)=>{
     let clientIp = req.headers['x-forwarded-for']
     const geop = geoip.lookup(`${clientIp}`.split(',')[0])
-    console.log(geoip.lookup('35.230.106.76'))
-    const log = `${new Date().toISOString()} ${req.method} ${req.url} GEO: ${geop?.country} IP: ${clientIp}`
+    const log = `${new Date().toISOString()} ${req.method} ${req.url} Country: ${geop?.country} City: ${geop?.city} IP: ${clientIp}`
     console.log(log)
     accessLogStream.write(`${log}\n`)
     next()
