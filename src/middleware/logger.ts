8c3f57799120251145da66a47f8e365f9de18dcf
Tahir Dibirov II <tahirdibirovresearch@gmail.com>
2023-02-07 17:35:32 +0400

    logger

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -5,8 +5,8 @@ import path from 'path'
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
 
 const logger = (req: Request, res: Response, next: NextFunction)=>{
-    const log = `${new Date().toISOString()} ${req.method} ${req.url} IP: ${req.ip}`
-    console.log(log)
+    const log = `${new Date().toISOString()} ${req.method} ${req.url} IP: ${req.socket.remoteAddress}`
+    console.log(log, )
     accessLogStream.write(`${log}\n`)
     next()
 }
